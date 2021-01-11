d3783fffb18dba29c2ef0f1c85dad643f41c795a
Santi <56729478+OtroCuliau@users.noreply.github.com>
2021-01-11 12:25:15 -0300

    Update login_validate.php

--- WiFi Connection with local server/login_validate.php
+++ WiFi Connection with local server/login_validate.php
@@ -1,18 +1,20 @@
 <?php
     // Inicio de sesiones
-    session_start();
+    session_start();    // Inicializo para crear variables de sesiones que permite compartir informacion del usuario 
+                        // con otros archivos .php
     // Me conecto a la BD
     require_once('conectar.php')                        ;
     $conectar   = conectar()                            ;
     $user       = strip_tags($_POST['user'])            ;
-    $password   = strip_tags(sha1($_POST['password']))  ;
+    $password   = strip_tags(sha1($_POST['password']))  ;   // Encripto el PW para compararlo con el que esta en la BD
 
-    $query = mysqli_query($conectar, 'SELECT * FROM usuarios WHERE user=<'. mysqli_real_escape_string($conectar, $user). 
-                          '" AND password="'. mysqli_real_escape_string($conectar, $password).'"');
+    $query = mysqli_query($conectar, 'SELECT * FROM usuarios WHERE user="'. mysqli_real_escape_string($conectar, $user).    // El real_escape se usa para mayor seguridad para evitar caracteres especiales en los datos ingresados
+                          '" AND password="'. mysqli_real_escape_string($conectar, $password).'"'); //Este query solo da resultado si el nombre y PW existen
 
+    // Si el usuario existe, se loggea 
     if($existe = mysqli_fetch_object($query))
     {
-        $hoy = date("Y-m-d H:i:s")
+        $hoy = date("Y-m-d H:i:s");
 
         $_SESSION['logged'] = 'yes';
 
@@ -37,12 +39,13 @@
 
         echo "true";
 
-        echo '<meta http-equiv="Refresh" content="3;url=http://www.cadena3.com">';
+        // Se usa este pedacito de codigo HTML para redirigirme a otra pagina web
+        echo '<meta http-equiv="Refresh" content="3;url=https://github.com/OtroCuliau">';   // Luego de 3 segundos me redirige a esa pag.
     }
     else
     {
         $_SESSION ['logged'] = 'no';
         echo "false";
     }
-    
+
 ?>
