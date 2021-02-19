1515febd39ede435ccb99d945aaa3f1c0b486ced
Santi <56729478+OtroCuliau@users.noreply.github.com>
2021-02-19 12:36:15 -0300

    Delete Pagina_web/my_php directory

--- Pagina_web/my_php/database_connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-// DESCOMENTAR PARA USAR LA BD DE INTERNET (obviamente comentar la linea de abajo)
-// $con=mysqli_connect("localhost","id15900605_santiyfer","_<F}(%^\Mn+L3}za","id15900605_esp8266");// server, user, password, database
-// DESCOMENTAR PARA USAR LA BD LOCAL (XAMPP) 
-$con=mysqli_connect("localhost","root","","id15900605_esp8266");// server, user, password, database
-
-?>
